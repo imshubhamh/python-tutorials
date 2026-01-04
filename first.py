03ae743158263e27b1f058fad9edfe6069fa5a7a
Subhash Sagar Singh <shubhamdev7317@example.com>
2026-01-04 18:31:35 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,33 @@
+
+# this is the hello world 
+# this is the hello world 
+
+
+# print("Hello World!")
+# print("Heloo Python!")
+
+# a=15
+# b=12.0
+# c="1"
+# d=2+4j
+
+# Integer Value 
+# print("This is value of a:",a)
+# print("This is value of b:",b)
+# type()
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+
+a=10
+b=2
+c=a/b
+
+print(c)
+
+
+
+
---
+++ l_5.py
@@ -0,0 +1,6 @@
+a=3+4j
+#octal
+print("value of a:",a)
+
+# 0-9 - digit 
+# A to F --alphabets
