57d60b75d5572927d57893047d3b4f5a111b1052
Subhash Sagar Singh <shubhamdev7317@example.com>
2026-01-04 19:34:50 +0530

    Lecture 7

---
+++ l_7.py
@@ -0,0 +1,21 @@
+# Immutable 
+# a = 10
+# print(a)
+# print(id(a))
+
+
+# a = 15
+# print(a)
+# print(id(a))
+
+
+# Mutable Data Type
+
+marks = [10, 20 , 30]
+print(marks)
+print(id(marks))
+
+
+marks[1] = 99
+print(marks)
+print(id(marks))
