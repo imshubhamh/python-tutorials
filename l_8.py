f51d7b30e3df519381b266efcf60acf4195380fd
Subhash Sagar Singh <shubhamdev7317@example.com>
2026-01-18 14:51:31 +0530

    Lecture-8

---
+++ l_8.py
@@ -0,0 +1,34 @@
+# # Airthmatic Operators:
+
+# a=20
+# b=4
+# c=a+b
+# d=a-b
+# e=a*b
+# f=a/b
+# g=a%b
+# h=a//b  
+# # floor
+# i=a**b
+# # exponents
+
+# print("Sum of a and b=", c)
+# print("Sub of a and b=", d)
+# print("Multiply of a and b=", e)
+# print("Devesion of a and b=", f)
+# print("Modul of a and b=", g)
+# print("Floor of a and b=", h)
+# print("Exponents of a and b=", i)
+
+
+# WAP to add two numbers enterd by the user.
+
+a = int(input("Enter the value of a:"))
+b = int(input("Enter the value of b:"))
+
+c = a + b
+
+print("Sum of two numbers (a and b):", c)
+
+
+
