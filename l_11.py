22dbc0fe71b26575b87cc2f03083c5c77a504a05
Subhash Sagar Singh <shubhamdev7317@example.com>
2026-01-18 16:05:23 +0530

    Lecture-10 and lecture-11

---
+++ l_11.py
@@ -0,0 +1,22 @@
+# Assignment Operator
+
+a = 10
+
+c = a + 5
+d = a - 5
+e = a * 5
+f = a / 5
+g = a // 5
+h = a ** 5
+
+# 10 += 5
+# 10 + 5 = 15
+
+print("value of c:",c)
+print("value of d:",d)
+print("value of e:",e)
+print("value of f:",f)
+print("value of g:",g)
+print("value of h:",h)
+
+
