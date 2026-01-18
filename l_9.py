09c359303bea933f28c9186468c0750fd82110e2
Subhash Sagar Singh <shubhamdev7317@example.com>
2026-01-18 15:19:57 +0530

    lecture-9

---
+++ l_9.py
@@ -0,0 +1,17 @@
+
+# Relational Operator
+
+# a = 5
+# b = 6
+# #  5 == 6
+# print(a != b)
+
+# 1. WAP to check which number is greater b/w two numbers.
+
+print("Finding Greater value :")
+a = int(input("Enter the value of a :"))
+b = int(input(" Enter the value of b :"))
+
+c = a>b
+print(c)
+
